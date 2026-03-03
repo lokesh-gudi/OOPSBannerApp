3b09f9624524b4555c4c49e71966cb885054320e
loskeh-gudi <lokeshgudi12@gmail.com>
2026-03-03 15:28:03 +0530

    UC1: Add printOOPsConcepts method

---
+++ src/OOPsBannerApp.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class OOPsBannerApp {
+    public static void main(String[] args) {
+        showMenu();
+    }
+    // UC1 - Print OOPs
+    public static void printOOPsConcepts() {
+        System.out.println("OOPs Concepts:");
+        System.out.println("1. Encapsulation");
+        System.out.println("2. Abstraction");
+        System.out.println("3. Inheritance");
+        System.out.println("4. Polymorphism");
+    }
+    // Temporary Menu (Will expand in next UC)
+    public static void showMenu() {
+        printOOPsConcepts();
+    }
+}
